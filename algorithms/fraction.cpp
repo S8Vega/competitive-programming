1a23a319b3ebbf083a572acd1c392f1967447c51
S8Vega <bsebastianvg18@gmail.com>
2021-05-01 12:23:45 -0500

    update struct fraction

--- algorithms/fraction.cpp
+++ algorithms/fraction.cpp
@@ -1,25 +1,55 @@
 // estructura para hacer operaciones entre fracciones
 
 struct Fraction {
-  ll n, d;
-  Fraction(ll _num = 0, ll _den = 1) {
-    n = _num;
-    d = _den;
+  ll u, d;
+
+  Fraction(ll _u = 0, ll _d = 1) {
+    u = _u, d = _d;
+    if (d) {
+      ll g = __gcd(abs(u), abs(d));
+      u /= g, d /= g;
+      if (d < 0) u = -u, d = -d;
+    } else
+      u = 1;
   }
-  Fraction operator+(const Fraction &b) {
-    fr c;
-    c.n = (n * b.d) + (d * b.n);
-    c.d = d * b.d;
-    ll g = __gcd(c.n, c.d);
-    c.n /= g, c.d /= g;
-    return c;
+
+  Fraction(double input) {
+    double integral = floor(input);
+    double frac = input - integral;
+
+    const ll precision = 1000000000;  // This is the accuracy.
+
+    ll gcd_ = gcd(round(frac * precision), precision);
+
+    d = precision / gcd_;
+    u = round(frac * precision) / gcd_;
   }
-  Fraction operator/(Fraction &b) {
-    fr c;
-    c.n = n * b.d;
-    c.d = d * b.n;
-    ll g = __gcd(c.n, c.d);
-    c.n /= g, c.d /= g;
-    return c;
+
+  void print() { cout << u << '/' << d << endl; }
+
+  Fraction operator+(const Fraction &f) const {
+    return Fraction(u * f.d + d * f.u, d * f.d);
   }
+
+  Fraction operator-(const Fraction &f) const {
+    return Fraction(u * f.d - d * f.u, d * f.d);
+  }
+
+  Fraction operator*(const Fraction &f) const {
+    return Fraction(u * f.u, d * f.d);
+  }
+
+  Fraction operator/(const Fraction &f) const {
+    return Fraction(u * f.d, d * f.u);
+  }
+
+  Fraction operator<(const Fraction &f) const { return u * f.d < d * f.u; }
+
+  Fraction operator<=(const Fraction &f) const { return u * f.d <= d * f.u; }
+
+  Fraction operator>(const Fraction &f) const { return u * f.d > d * f.u; }
+
+  Fraction operator>=(const Fraction &f) const { return u * f.d >= d * f.u; }
+
+  Fraction operator==(const Fraction &f) const { return u * f.d == d * f.u; }
 };
