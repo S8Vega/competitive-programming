7747d4a810511fc983d15f0720d5aa9182ae2e90
Sebas <bsebastianvg18@gmail.com>
2021-07-31 08:13:59 -0500

    add hackerrank problem print_the_elements_of_a_linked_list

---
+++ problems/hackerrank/print_the_elements_of_a_linked_list.java
@@ -0,0 +1,73 @@
+import java.util.Scanner;
+
+public class print_the_elements_of_a_linked_list {
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    /*
+     * For your reference:
+     *
+     * SinglyLinkedListNode {
+     *     int data;
+     *     SinglyLinkedListNode next;
+     * }
+     *
+     */
+    static void printLinkedList(SinglyLinkedListNode head) {
+        if (head == null) return;
+        System.out.println(head.data);
+        printLinkedList(head.next);
+    }
+
+    // Complete the printLinkedList function below.
+
+    public static void main(String[] args) {
+        SinglyLinkedList llist = new SinglyLinkedList();
+
+        int llistCount = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < llistCount; i++) {
+            int llistItem = scanner.nextInt();
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+            llist.insertNode(llistItem);
+        }
+
+        printLinkedList(llist.head);
+
+        scanner.close();
+    }
+
+    static class SinglyLinkedListNode {
+        public int data;
+        public SinglyLinkedListNode next;
+
+        public SinglyLinkedListNode(int nodeData) {
+            this.data = nodeData;
+            this.next = null;
+        }
+    }
+
+    static class SinglyLinkedList {
+        public SinglyLinkedListNode head;
+        public SinglyLinkedListNode tail;
+
+        public SinglyLinkedList() {
+            this.head = null;
+            this.tail = null;
+        }
+
+        public void insertNode(int nodeData) {
+            SinglyLinkedListNode node = new SinglyLinkedListNode(nodeData);
+
+            if (this.head == null) {
+                this.head = node;
+            } else {
+                this.tail.next = node;
+            }
+
+            this.tail = node;
+        }
+    }
+}
