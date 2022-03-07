aef28041667f27ef6a4d0523a60b7ed87cd675cf
S8Vega <bsebastianvg18@gmail.com>
2022-03-07 18:30:50 -0500

    add codeforces round 771 problem

---
+++ problems/Codeforces Round #771 (Div. 2)/c.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> pii;
+#define forn(i, a, b) for (int i = int(a); i < int(b); ++i)
+#define SZ(x) int(x.size())
+#define pb push_back
+#define F first
+#define S second
+#define endl '\n'
+#define dbg(...) cerr << "(" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
+void dbg_out() { cerr << endl; }
+template <typename Head, typename... Tail>
+void dbg_out(Head H, Tail... T) {
+  cerr << ' ' << H;
+  dbg_out(T...);
+}
+
+void solve() {
+  ll n;
+  cin >> n;
+  vector<ll> a(n);
+  forn(i, 0, n) cin >> a[i], a[i]--;
+
+  ll ans = 0, mx = -1;
+
+  forn(i, 0, n) {
+    mx = max(mx, a[i]);
+
+    if (mx == i) {
+      ans++;
+      mx = -1;
+    }
+  }
+  cout << ans << endl;
+}
+
+int main() {
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  int t;
+  cin >> t;
+  while (t--) {
+    solve();
+  }
+  return 0;
+}
+/*
+g++ -pthread -Wall -Wshadow -std=c++17 -o main main.cpp && ./main < main.txt
+freopen("input.in", "r" , stdin);
+freopen("output.out", "w", stdout);
+
+g++ -DLOCAL -std=c++17 -g -O2 -Wconversion -Wshadow -Wall -Wextra
+-D_GLIBCXX_DEBUG -o main main.cpp && for x in main*.txt; do echo
+====================$x=================; time -p ./main <$x; echo
+===========================================; done
+*/
