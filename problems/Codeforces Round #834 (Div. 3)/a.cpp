4d952be0cde0a782019b7c813ad851e473efb2b1
S8Vega <bsebastianvg18@gmail.com>
2022-11-19 16:35:52 -0500

    add problem a of codeforces round 834 contest

---
+++ problems/Codeforces Round #834 (Div. 3)/a.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> pii;
+#define forn(i, a, b) for (int i = int(a); i < int(b); ++i)
+#define SZ(x) int(x.size())
+#define pb push_back
+#define F first
+#define S second
+#define endl '\n'
+#define dbg(...) cerr << "(" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
+void dbg_out() { cerr << endl; }
+template <typename Head, typename... Tail>
+void dbg_out(Head H, Tail... T) {
+  cerr << ' ' << H;
+  dbg_out(T...);
+}
+
+const string PATH = "Yes";
+
+void solve() {
+  string s;
+  cin >> s;
+  string t = "";
+  forn(i, 0, 100) t += PATH;
+  forn(i, 0, SZ(t) - SZ(s) + 1) if (s == t.substr(i, SZ(s))) {
+    cout << "YES" << endl;
+    return;
+  }
+  cout << "NO" << endl;
+}
+
+int main() {
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  int t;
+  cin >> t;
+  while (t--) {
+    solve();
+  }
+  return 0;
+}
+/*
+g++ -pthread -Wall -Wshadow -std=c++17 -o main main.cpp && ./main < main.txt
+freopen("input.in", "r" , stdin);
+freopen("output.out", "w", stdout);
+
+g++ -DLOCAL -std=c++17 -g -O2 -Wconversion -Wshadow -Wall -Wextra
+-D_GLIBCXX_DEBUG -o main main.cpp && for x in main*.txt; do echo
+====================$x=================; time -p ./main <$x; echo
+===========================================; done
+*/
