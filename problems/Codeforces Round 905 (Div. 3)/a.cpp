a96dfc864c4256421f9c35ddb621cbb8b14fcef4
S8Vega <bsebastianvg18@gmail.com>
2023-10-29 14:36:58 -0500

    add Codeforces Round 905 (Div. 3) problems

---
+++ problems/Codeforces Round 905 (Div. 3)/a.cpp
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> pii;
+#define forn(i, a, b) for (int i = int(a); i < int(b); ++i)
+#define SZ(x) int(x.size())
+#define pb push_back
+#define F first
+#define S second
+#define endl '\n'
+#define dbg(...) cerr << "(" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
+void dbg_out() { cerr << endl; }
+template <typename Head, typename... Tail>
+void dbg_out(Head H, Tail... T) {
+  cerr << ' ' << H;
+  dbg_out(T...);
+}
+
+void solve() {
+  int ans = 0, curr, last = 1;
+  string s;
+  cin >> s;
+  forn(i, 0, SZ(s)) {
+    curr = s[i] - '0';
+    curr = curr == 0 ? 10 : curr;
+    ans += abs(curr - last) + 1;
+    last = curr == 0 ? 10 : curr;
+  }
+  cout << ans << endl;
+}
+
+int main() {
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  int t;
+  cin >> t;
+  while (t--) {
+    solve();
+  }
+  return 0;
+}
+/*
+g++ -pthread -Wall -Wshadow -std=c++17 -o main main.cpp && ./main < main.txt
+freopen("input.in", "r" , stdin);
+freopen("output.out", "w", stdout);
+
+g++ -DLOCAL -std=c++17 -g -O2 -Wconversion -Wshadow -Wall -Wextra
+-D_GLIBCXX_DEBUG -o main main.cpp && for x in main*.txt; do echo
+====================$x=================; time -p ./main <$x; echo
+===========================================; done
+*/
---
+++ problems/Codeforces Round 905 (Div. 3)/b.cpp
@@ -0,0 +1,82 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> pii;
+#define forn(i, a, b) for (int i = int(a); i < int(b); ++i)
+#define SZ(x) int(x.size())
+#define pb push_back
+#define F first
+#define S second
+#define endl '\n'
+#define dbg(...) cerr << "(" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
+void dbg_out() { cerr << endl; }
+template <typename Head, typename... Tail>
+void dbg_out(Head H, Tail... T) {
+  cerr << ' ' << H;
+  dbg_out(T...);
+}
+
+struct CustomComparator {
+  bool operator()(int a, int b) {
+    if (a % 2 != b % 2)
+      return a % 2 < b % 2;
+    else
+      return a > b;
+  }
+};
+
+bool f(int k, string s) {
+  priority_queue<int, vector<int>, CustomComparator> pq;
+  map<char, int> rep;
+  for (char c : s) {
+    rep[c]++;
+  }
+  for (auto x : rep) {
+    pq.push(x.S);
+  }
+  int last;
+  while (k-- > 0) {
+    last = pq.top();
+    // dbg(k, last);
+    pq.pop();
+    last--;
+    if (last > 0) pq.push(last);
+  }
+  if (!(pq.top() % 2)) return true;
+  pq.pop();
+  return pq.empty() || !(pq.top() % 2);
+}
+
+void solve() {
+  int n, k;
+  string s;
+  cin >> n >> k >> s;
+  bool ans = f(k, s);
+  // dbg(n, k, s, ans);
+  if (ans)
+    cout << "YES" << endl;
+  else
+    cout << "NO" << endl;
+}
+
+int main() {
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  int t;
+  cin >> t;
+  while (t--) {
+    solve();
+  }
+  return 0;
+}
+/*
+g++ -pthread -Wall -Wshadow -std=c++17 -o main main.cpp && ./main < main.txt
+freopen("input.in", "r" , stdin);
+freopen("output.out", "w", stdout);
+
+g++ -DLOCAL -std=c++17 -g -O2 -Wconversion -Wshadow -Wall -Wextra
+-D_GLIBCXX_DEBUG -o main main.cpp && for x in main*.txt; do echo
+====================$x=================; time -p ./main <$x; echo
+===========================================; done
+*/
