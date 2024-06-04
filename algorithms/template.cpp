9673bb15b70b2c28e9884c1f6817074ae29c6339
S8Vega <bsebastianvg18@gmail.com>
2024-06-03 23:05:14 -0500

    add new command for run in windows powershell

--- algorithms/template.cpp
+++ algorithms/template.cpp
@@ -34,8 +34,16 @@ g++ -pthread -Wall -Wshadow -std=c++17 -o main main.cpp && ./main < main.txt
 freopen("input.in", "r" , stdin);
 freopen("output.out", "w", stdout);
 
+Linux command
 g++ -DLOCAL -std=c++17 -g -O2 -Wconversion -Wshadow -Wall -Wextra
 -D_GLIBCXX_DEBUG -o main main.cpp && for x in main*.txt; do echo
 ====================$x=================; time -p ./main <$x; echo
 ===========================================; done
+
+Powershell command
+cd $dir && g++ -DLOCAL -std=c++17 -g -O2 -Wconversion -Wshadow -Wall -Wextra -o
+main $fileName && $testcase = '>>> name'; foreach ($file in Get-ChildItem
+$fileNameWithoutExt*.txt) { echo  $testcase.Replace('name', $file.Name);
+Get-Content $file.Name | ./main; echo
+==========================================; }
 */
