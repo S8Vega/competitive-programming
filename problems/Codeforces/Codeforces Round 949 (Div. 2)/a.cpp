65333a5f582dcd43d2ad22d9f6f05198104c231b
S8Vega <bsebastianvg18@gmail.com>
2024-06-05 08:33:52 -0500

    add codeforces round 949 problems

--- problems/Codeforces/Codeforces Round 949 (Div. 2)/a.cpp
+++ problems/Codeforces/Codeforces Round 949 (Div. 2)/a.cpp
@@ -17,7 +17,21 @@ void dbg_out(Head H, Tail... T) {
   dbg_out(T...);
 }
 
-void solve() {}
+void solve() {
+  int l, r;
+  cin >> l >> r;
+  ll a[32], x = 1;
+  forn(i, 0, 32) {
+    a[i] = x;
+    x = x * 2;
+  }
+  forn(i, 0, 32) {
+    if (a[i] > r) {
+      cout << i - 1 << endl;
+      return;
+    }
+  }
+}
 
 int main() {
   ios::sync_with_stdio(0);
@@ -29,22 +43,13 @@ int main() {
   }
   return 0;
 }
-
 /*
 g++ -pthread -Wall -Wshadow -std=c++17 -o main main.cpp && ./main < main.txt
 freopen("input.in", "r" , stdin);
 freopen("output.out", "w", stdout);
 
-Linux command
 g++ -DLOCAL -std=c++17 -g -O2 -Wconversion -Wshadow -Wall -Wextra
 -D_GLIBCXX_DEBUG -o main main.cpp && for x in main*.txt; do echo
 ====================$x=================; time -p ./main <$x; echo
 ===========================================; done
-
-Powershell command
-cd $dir && g++ -DLOCAL -std=c++17 -g -O2 -Wconversion -Wshadow -Wall -Wextra -o
-main $fileName && $testcase = '>>> name'; foreach ($file in Get-ChildItem
-$fileNameWithoutExt*.txt) { echo  $testcase.Replace('name', $file.Name);
-Get-Content $file.Name | ./main; echo
-==========================================; }
 */
--- problems/Codeforces/Codeforces Round 949 (Div. 2)/b.cpp
+++ problems/Codeforces/Codeforces Round 949 (Div. 2)/b.cpp
@@ -17,11 +17,39 @@ void dbg_out(Head H, Tail... T) {
   dbg_out(T...);
 }
 
-void solve() {}
+void solve() {
+  ll n, m;
+  cin >> n >> m;
+  ll l = max(0LL, n - m), r = n + m, ans = 0;
+  for (int i = 31; ~i; --i) {
+    if ((l & (1LL << i)) || (r & (1LL << i)) ||
+        (l >> (i + 1)) != (r >> (i + 1))) {
+      ans |= (1LL << i);
+    }
+  }
+  cout << ans << endl;
+}
+
+void init() {
+  int n = 35;
+  int a[n];
+  forn(i, 0, n) { a[i] = i; }
+  forn(i, 0, n) {
+    cout << ">>> " << (i < 10 ? "0" : "") << i << ": ";
+    forn(j, 0, n) { cout << (a[j] < 10 ? "0" : "") << a[j] << " "; }
+    cout << endl;
+    int b[n];
+    b[0] = a[0] | a[1];
+    forn(j, 1, n - 1) { b[j] = a[j - 1] | a[j] | a[j + 1]; }
+    b[n - 1] = a[n - 1] | a[n - 2];
+    forn(j, 0, n) { a[j] = b[j]; }
+  }
+}
 
 int main() {
   ios::sync_with_stdio(0);
   cin.tie(0);
+  // init();
   int t;
   cin >> t;
   while (t--) {
@@ -29,7 +57,6 @@ int main() {
   }
   return 0;
 }
-
 /*
 g++ -pthread -Wall -Wshadow -std=c++17 -o main main.cpp && ./main < main.txt
 freopen("input.in", "r" , stdin);
