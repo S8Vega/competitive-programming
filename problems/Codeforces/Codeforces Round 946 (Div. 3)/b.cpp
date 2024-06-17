05721447a2b7242c72e7a778c10f10cce3c28063
S8Vega <bsebastianvg18@gmail.com>
2024-06-16 22:28:54 -0500

    add problems of round 946

---
+++ problems/Codeforces/Codeforces Round 946 (Div. 3)/b.cpp
@@ -0,0 +1,71 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> pii;
+#define forn(i, a, b) for (int i = int(a); i < int(b); ++i)
+#define SZ(x) int(x.size())
+#define pb push_back
+#define F first
+#define S second
+#define endl '\n'
+#define dbg(...) cerr << "(" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
+void dbg_out() { cerr << endl; }
+template <typename Head, typename... Tail>
+void dbg_out(Head H, Tail... T) {
+  cerr << ' ' << H;
+  dbg_out(T...);
+}
+
+void solve() {
+  int n;
+  string s;
+  cin >> n >> s;
+  set<char> st;
+  for (char c : s) {
+    st.insert(c);
+  }
+  string tmp = "";
+  for (char c : st) {
+    tmp += c;
+  }
+  // dbg(tmp);
+  map<char, char> mp;
+  for (int i = 0; i < SZ(tmp); i++) {
+    mp[tmp[i]] = tmp[SZ(tmp) - i - 1];
+  }
+  for (char c : s) {
+    cout << mp[c];
+  }
+  cout << endl;
+}
+
+int main() {
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  int t;
+  cin >> t;
+  while (t--) {
+    solve();
+  }
+  return 0;
+}
+
+/*
+g++ -pthread -Wall -Wshadow -std=c++17 -o main main.cpp && ./main < main.txt
+freopen("input.in", "r" , stdin);
+freopen("output.out", "w", stdout);
+
+Linux command
+g++ -DLOCAL -std=c++17 -g -O2 -Wconversion -Wshadow -Wall -Wextra
+-D_GLIBCXX_DEBUG -o main main.cpp && for x in main*.txt; do echo
+====================$x=================; time -p ./main <$x; echo
+===========================================; done
+
+Powershell command
+cd $dir && g++ -DLOCAL -std=c++17 -g -O2 -Wconversion -Wshadow -Wall -Wextra -o
+main $fileName && $testcase = '>>> name'; foreach ($file in Get-ChildItem
+$fileNameWithoutExt*.txt) { echo  $testcase.Replace('name', $file.Name);
+Get-Content $file.Name | ./main; echo
+==========================================; }
+*/
