3d4d896d7f0ad40dc0a1bb9d4ba2d159f8f89280
S8Vega <bsebastianvg18@gmail.com>
2025-01-04 14:10:23 -0500

    Agregar solución para el Educational Codeforces Round 173 (Rated for Div. 2)

---
+++ problems/Codeforces/Educational Codeforces Round 173 (Rated for Div. 2)/a.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> pii;
+#define forn(i, a, b) for (int i = int(a); i < int(b); ++i)
+#define SZ(x) int(x.size())
+#define pb push_back
+#define F first
+#define S second
+#define endl '\n'
+#define dbg(...) cerr << "(" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
+void dbg_out() { cerr << endl; }
+template <typename Head, typename... Tail>
+void dbg_out(Head H, Tail... T) {
+  cerr << ' ' << H;
+  dbg_out(T...);
+}
+
+void solve() {
+  ll n, ans = 1;
+  cin >> n;
+  while (n > 3) {
+    n /= 4;
+    ans *= 2;
+  }
+  cout << ans << endl;
+}
+
+int main() {
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  int t;
+  cin >> t;
+  while (t--) {
+    solve();
+  }
+  return 0;
+}
